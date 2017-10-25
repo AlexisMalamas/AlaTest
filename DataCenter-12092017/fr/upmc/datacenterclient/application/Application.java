d2ebd9530d315205c85ce9fa28bc1583f32aad63
alexis <alexis@132.227.129.224>
2017-10-25 16:11:05 +0200

    start application

--- DataCenter-12092017/fr/upmc/datacenterclient/application/Application.java
+++ DataCenter-12092017/fr/upmc/datacenterclient/application/Application.java
@@ -2,10 +2,15 @@ package fr.upmc.datacenterclient.application;
 
 import fr.upmc.components.AbstractComponent;
 import fr.upmc.components.exceptions.ComponentShutdownException;
+import fr.upmc.datacenter.software.connectors.RequestNotificationConnector;
+import fr.upmc.datacenter.software.connectors.RequestSubmissionConnector;
 import fr.upmc.datacenterclient.application.interfaces.ApplicationSubmissionI;
 import fr.upmc.datacenterclient.application.ports.ApplicationSubmissionOutboundPort;
+import fr.upmc.datacenterclient.requestgenerator.RequestGenerator;
+import fr.upmc.datacenterclient.requestgenerator.connectors.RequestGeneratorManagementConnector;
 import fr.upmc.datacenterclient.requestgenerator.interfaces.RequestGeneratorManagementI;
 import fr.upmc.datacenterclient.requestgenerator.ports.RequestGeneratorManagementInboundPort;
+import fr.upmc.datacenterclient.requestgenerator.ports.RequestGeneratorManagementOutboundPort;
 
 /**
  * 
@@ -20,6 +25,8 @@ implements ApplicationSubmissionI{
 	// the uri application
 	protected final String applicationURI;
 	
+	/**Request generator component.*/
+	protected RequestGenerator rg ;
 	
 	/** the outbound port provided to manage the component.	*/
 	protected RequestGeneratorManagementInboundPort rgmop ;
@@ -42,13 +49,43 @@ implements ApplicationSubmissionI{
 		this.rgmop = new RequestGeneratorManagementInboundPort(RequestGeneratorManagementInboundPort, this) ;
 		this.addPort(this.rgmop) ;
 		this.rgmop.publishPort() ;
+		
+		
 	}
 
 	@Override
 	public void submissionApplication() {
 		// TODO Auto-generated method stub
-		
-		//create the request generator
+		/*
+		// --------------------------------------------------------------------
+		// Creating the request generator component.
+		// --------------------------------------------------------------------
+		this.rg = new RequestGenerator(
+					"rg",			// generator component URI
+					500.0,			// mean time between two requests
+					6000000000L,	// mean number of instructions in requests
+					RequestGeneratorManagementInboundPortURI,
+					RequestSubmissionOutboundPortURI,
+					RequestNotificationInboundPortURI) ;
+		this.addDeployedComponent(rg) ;
+
+		// Toggle on tracing and logging in the request generator to
+		// follow the submission and end of execution notification of
+		// individual requests.
+		this.rg.toggleTracing() ;
+		this.rg.toggleLogging() ;
+
+		// Create a mock up port to manage to request generator component
+		// (starting and stopping the generation).
+		this.rgmop = new RequestGeneratorManagementOutboundPort(
+							RequestGeneratorManagementOutboundPortURI,
+							new AbstractComponent(0, 0) {}) ;
+		this.rgmop.publishPort() ;
+		this.rgmop.doConnection(
+				RequestGeneratorManagementInboundPortURI,
+				RequestGeneratorManagementConnector.class.getCanonicalName()) ;
+		// --------------------------------------------------------------------
+		*/
 	}
 
 	@Override
